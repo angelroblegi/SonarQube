d4b88d4df64f26e015ea694dd48c30da423ab192
angelroblegi <72879748+angelroblegi@users.noreply.github.com>
2025-06-09 10:13:37 -0500

    Update app.py
    Fix table

--- pages/app.py
+++ pages/app.py
@@ -174,7 +174,7 @@ agrupado = agrupado.round(1).reset_index()
 
 agrupado.columns = [
     'Célula', '% Security', '% Reliability', '% Maintainability',
-    '% Coverage', '% Duplication', 'Duplications (%)', 'Duplications (%)', 'Bugs', 'Blocker',
+    '% Coverage', '% Duplication', 'Duplications (%)', 'Bugs', 'Blocker',
     'Critical', 'Major', 'Minor', 'Info'
 ]
 
